cdb93881ef49e6dde28f4e34dfdba38cb182b5a7
dorival <dpiedade@sc.usp.br>
2025-05-07 16:47:58 -0300

    Creating the module where tests to be used during development are

---
+++ src/test/dev_tests.f90
@@ -0,0 +1,17 @@
+! First tests (during development)
+
+module dev_tests
+    use sparse_set
+    implicit none
+
+
+contains
+
+    subroutine test_allocate_line()
+        implicit none
+        write(*,*)"Just starting to write the code..."         
+
+    end subroutine test_allocate_line
+
+module dev_tests
+
