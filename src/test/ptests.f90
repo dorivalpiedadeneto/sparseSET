e75a98b1a8dc6fcf2dccc690ef7b877ba775a1eb
dorival <dpiedade@sc.usp.br>
2025-05-23 19:04:00 -0300

    Testing IA solution to understand the algorithm

--- src/test/ptests.f90
+++ src/test/ptests.f90
@@ -9,6 +9,7 @@ program ptest
     integer(spip),dimension(:), allocatable::indexes
 
     call test_indexes_creation()
+    call test_quicksort()
 
     contains
 
@@ -62,5 +63,76 @@ program ptest
         allocate(ind_arr(n))
         ind_arr(1:n) = ind_src(1:n)
     end subroutine
+
+    ! ChatGPT suggested algorithm (to help finding error in the adapted
+    ! Numerical Recipes algorithm)
+    recursive subroutine quicksort_indices_sub(values, idx, low, high)
+        implicit none
+        integer, intent(in) :: low, high
+        integer, dimension(:), intent(in) :: values
+        integer, dimension(:), intent(inout) :: idx
+        integer :: pivotIndex
+
+        if (low < high) then
+            call partition_indices(values, idx, low, high, pivotIndex)
+            call quicksort_indices_sub(values, idx, low, pivotIndex - 1)
+            call quicksort_indices_sub(values, idx, pivotIndex + 1, high)
+        end if
+    end subroutine quicksort_indices_sub
+
+    !------------------------------------------------------------
+    subroutine partition_indices(values, idx, low, high, pivotIndex)
+        implicit none
+        integer, intent(in) :: low, high
+        integer, dimension(:), intent(in) :: values
+        integer, dimension(:), intent(inout) :: idx
+        integer, intent(out) :: pivotIndex
+        integer :: i, j, temp, pivotValue
+
+        pivotValue = values(idx(high))
+        i = low - 1
+
+        do j = low, high - 1
+            if (values(idx(j)) <= pivotValue) then
+                i = i + 1
+                temp = idx(i)
+                idx(i) = idx(j)
+                idx(j) = temp
+            end if
+        end do
+
+        temp = idx(i + 1)
+        idx(i + 1) = idx(high)
+        idx(high) = temp
+
+        pivotIndex = i + 1
+    end subroutine partition_indices
+
+
+    subroutine test_quicksort()
+        implicit none
+        integer, dimension(:), allocatable::in_, out_, exp_, ind_
+        integer::i, n
+        real(8)::ti, tf
+
+        n = 10000
+
+        allocate(in_(n),out_(n), exp_(n))
+        do i=1,n
+            in_(i) = n + 1 - i
+            exp_(i) = n + 1 - i ! expected result
+            out_(i) = i
+        enddo
+
+        call cpu_time(ti)
+        call quicksort_indices_sub(in_, out_, 1, n)
+        call cpu_time(tf)
+
+        write(*,*)"Testing quicksort: worked? (T/F?) ->",&
+        (all(out_.eq.exp_))
+        write(*,*)"Time to sort ",n," terms:",(tf-ti)," (s)"
+
+
+    end subroutine test_quicksort
 
 end program ptest
